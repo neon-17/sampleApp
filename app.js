d0621b5965694f9950ef0fd51c5e352037697c1d
Dharmesh <dharmesh.convegenius@gmail.com>
2016-08-04 16:50:19 +0530

    app.js changed

--- app.js
+++ app.js
@@ -1,21 +1,4 @@
 var http = require("http");
-var server = http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("<!DOCTYPE 'html'>");
-  response.write("<html>");
-  response.write("<head>");
-  response.write("<title>Hello World Page</title>");
-  response.write("</head>");
-  response.write("<body>");
-  response.write("Hello World!");
-  response.write("</body>");
-  response.write("</html>");
-  response.end();
-});
-server.listen(80);
-console.log('Server is Listening');
-
-/*var http = require("http");
 var url = require("url");
 
 http.createServer(function(request, response){
@@ -35,4 +18,3 @@ http.createServer(function(request, response){
     response.write(sum);
     response.end();
 }).listen(3000);
-*/
