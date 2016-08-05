5eac9ad345628021be868e44ed139d93f01ac8d4
Dharmesh <dharmesh.convegenius@gmail.com>
2016-08-05 13:23:22 +0530

    port changed

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORTVAL || 8081;
+var port = process.env.PORTVAL || 3000;
+var AWS = require('aws-sdk');
+var credentials = new AWS.SharedIniFileCredentials({
+        profile: 'eb-cli'
+    });
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('My first Application!');
 });
 
 app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
+console.log(credentials);
