971b757dae810f1240fbd30e97e6e6d4d8872180
Dharmesh <dharmesh.convegenius@gmail.com>
2016-08-05 16:15:43 +0530

    trial2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORTVAL || 8081;
+var port = process.env.PORTVAL || 3000;
 var AWS = require('aws-sdk');
 var credentials = new AWS.SharedIniFileCredentials({
         profile: 'eb-cli'
