fe5ce82108cf2f8a5079986f9ec0b0971681331c
Dharmesh <dharmesh.convegenius@gmail.com>
2016-08-23 16:50:41 +0530

    default port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORTVAL || 3000;
+var port = process.env.PORTVAL || 8080;
 app.get('/', function (req, res) {
   res.send('My first Application!');
 });
