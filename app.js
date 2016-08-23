33076eebc85cc651b7527ca756312164422acbb5
Dharmesh <dharmesh.convegenius@gmail.com>
2016-08-23 16:15:45 +0530

    deployment on heroku

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORTVAL || 3000;
-var AWS = require('aws-sdk');
-var credentials = new AWS.SharedIniFileCredentials({
-        profile: 'eb-cli'
-    });
 app.get('/', function (req, res) {
   res.send('My first Application!');
 });
@@ -12,4 +8,3 @@ app.get('/', function (req, res) {
 app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
-console.log(credentials);
