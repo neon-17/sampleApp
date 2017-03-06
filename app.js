134458cc0c927a37d41314b945ff1d6ba41acc3a
dharmesh <dharmesh.convegenius@gmail.com>
2017-03-06 16:07:02 +0530

    logs at custom location

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
+var logspath = '/var/log/nodejs/logs-winston.log';
+var winston = require('winston');
+var logger = new winston.Logger({
+  level: 'info',
+  transports: [
+    new (winston.transports.Console)(),
+    new (winston.transports.File)({ filename: logspath })
+  ]
+});
 app.get('/', function (req, res) {
+    logger.info('Request recieved');
   res.send('My first Demo Application!');
 });
 
 app.listen(port, function () {
-  console.log('Example app listening on port '+ port+'!');
+  logger.info('Example app listening on port '+ port+'!');
 });
