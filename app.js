ecbc009372c04f6500f9c39bb8d62909fc60600c
dharmesh <dharmesh.convegenius@gmail.com>
2017-03-08 12:49:35 +0530

    cloudwatch logs

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var winston = require('winston');
 var logger = new winston.Logger({
   level: 'info',
   transports: [
-    new (winston.transports.Console)(),
     new (winston.transports.File)({ filename: logspath })
   ]
 });
